26aa13483aff206b3c39f43c59e16dbf74d658ae
Renan <renandasilva16@gmail.com>
2020-04-24 16:39:51 -0300

    Ajustando estrutura do MarketData

--- infrastructure/dtos/marketDataDto.go
+++ infrastructure/dtos/marketDataDto.go
@@ -1,17 +1,15 @@
 package dtos
 
 type MarketData struct {
-	Name           string
-	Sector         string
-	SubSector      string
-	Segmentation   string
-	B3Segmentation string
-	TagAlong       string
-	FreeFloat      string
-	Stocks         []Stock
-	BalanceSheet   BalanceSheet
-	Results        []Result
-	Market         Market
+	Name           string       
+	Sector         string       
+	SubSector      string       
+	Segmentation   string       
+	B3Segmentation string       
+	TagAlong       string       
+	FreeFloat      string      
+	Stocks         []Stock      
+	Results        []Result     
 }
 
 type Stock struct {
@@ -29,21 +27,27 @@ type BalanceSheet struct {
 	NetDebt    float64
 }
 
+type OperatingResult struct {
+	NetIncome                   float64 
+	BookBalance                 float64 
+	EBITDA                      float64 
+	DepreciationAndAmortization float64 
+	EBIT                        float64 
+	NetProfit                   float64 
+}
+
 type Result struct {
-	Date                        string
-	NetIncome                   float64
-	BookBalance                 float64
-	EBITDA                      float64
-	DepreciationAndAmortization float64
-	EBIT                        float64
-	NetProfit                   float64
-	FinancialIndicators FinancialIndicators
+	Date                string               
+	BalanceSheet   		BalanceSheet         
+	OperatingResults    OperatingResult     
+	Market         		Market              
+	FinancialIndicators FinancialIndicators 
 }
 
 type Market struct {
 	MarketValue     float64
 	EnterpriseValue float64
-	StocksCount     float64
+	StocksCount          float64
 }
 
 type MarketIndicators struct{
