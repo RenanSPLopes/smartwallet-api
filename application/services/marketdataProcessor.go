b55db0c6d4025f56ffee567931766fab6d161479
Renan <renandasilva16@gmail.com>
2020-05-30 01:09:34 -0300

    Implementando validação para q não tenha resultados duplicados

--- application/services/marketdataProcessor.go
+++ application/services/marketdataProcessor.go
@@ -1,9 +1,12 @@
 package services
 
 import (
+	"log"
 	"smartwallet-api/application/models"
 	"smartwallet-api/domain/entities"
+	"smartwallet-api/infrastructure/dtos"
 	"smartwallet-api/infrastructure/repositories"
+	"smartwallet-api/utils"
 
 	mapper "github.com/PeteProgrammer/go-automapper"
 )
@@ -23,49 +26,64 @@ func NewMarketDataProcessorService(m repositories.MarketDataRepository) MarketDa
 func (m MarketDataProcessorService) Process(marketDataModel models.MarketData) {
 	var marketData entities.MarketData
 	mapper.MapLoose(marketDataModel, &marketData)
+	marketData.CalculateResultIndicators()
 
 	marketDataFromDb := m.MarketDataRepository.GetByCode(marketData.Stocks[0].Code)
 
 	if marketDataFromDb.Name != "" {
-		marketData.CalculateResultIndicators()
-		m.MarketDataRepository.UpdateResults(marketDataFromDb.ID, marketData)
+		result := marketData.Results[0]
+		dates := extractResultsDates(marketDataFromDb.Results)
+
+		if utils.Contains(dates, marketData.Results[0].Date) {
+			log.Println("Result already exist on database.")
+			return
+		}
+
+		m.MarketDataRepository.UpdateResults(marketDataFromDb.ID, result)
 		return
 	}
 
-	marketData.CalculateResultIndicators()
 	marketData.CalculateStocksIndicators()
-
 	m.MarketDataRepository.Save(marketData)
 }
 
-func mapMarketFromModel(marketModel models.Market) entities.Market {
-	var market entities.Market
-	mapper.Map(marketModel, &market)
-	return market
-}
-
-func mapResultFromModel(resultsModel []models.Result) []entities.Result {
-	var results []entities.Result
-	for _, r := range resultsModel {
-		var result entities.Result
-		mapper.MapLoose(r, &result)
-		results = append(results, result)
+func extractResultsDates(results []dtos.Result) []string {
+	var dates []string
+	for _, x := range results {
+		dates = append(dates, x.Date)
 	}
 
-	return results
+	return dates
 }
 
-func mapStockFromModel(stocksModel []models.Stock) []entities.Stock {
-	var stocks []entities.Stock
-	for _, stockModel := range stocksModel {
-		stock := entities.Stock{
-			Code:   stockModel.Code,
-			Type:   stockModel.Type,
-			Quotes: stockModel.Quotes,
-		}
+// func mapMarketFromModel(marketModel models.Market) entities.Market {
+// 	var market entities.Market
+// 	mapper.Map(marketModel, &market)
+// 	return market
+// }
 
-		stocks = append(stocks, stock)
-	}
+// func mapResultFromModel(resultsModel []models.Result) []entities.Result {
+// 	var results []entities.Result
+// 	for _, r := range resultsModel {
+// 		var result entities.Result
+// 		mapper.MapLoose(r, &result)
+// 		results = append(results, result)
+// 	}
 
-	return stocks
-}
+// 	return results
+// }
+
+// func mapStockFromModel(stocksModel []models.Stock) []entities.Stock {
+// 	var stocks []entities.Stock
+// 	for _, stockModel := range stocksModel {
+// 		stock := entities.Stock{
+// 			Code:   stockModel.Code,
+// 			Type:   stockModel.Type,
+// 			Quotes: stockModel.Quotes,
+// 		}
+
+// 		stocks = append(stocks, stock)
+// 	}
+
+// 	return stocks
+// }
--- infrastructure/repositories/mongoDBMarketDataRepository.go
+++ infrastructure/repositories/mongoDBMarketDataRepository.go
@@ -20,7 +20,7 @@ type MarketDataRepository interface {
 	GetAll() []dtos.MarketData
 	GetById(id string) dtos.MarketData
 	GetByCode(code string) dtos.MarketData
-	UpdateResults(id primitive.ObjectID, marketData entities.MarketData)
+	UpdateResults(id primitive.ObjectID, result entities.Result)
 }
 
 type MongoDBMarketDataRepository struct {
@@ -112,14 +112,14 @@ func (m MongoDBMarketDataRepository) GetByCode(code string) dtos.MarketData {
 	return result
 }
 
-func (m MongoDBMarketDataRepository) UpdateResults(_id primitive.ObjectID, marketData entities.MarketData) {
+func (m MongoDBMarketDataRepository) UpdateResults(_id primitive.ObjectID, result entities.Result) {
 	ctx, client, marketDataCollection := m.GetMarketDataCollection()
 	defer client.Disconnect(ctx)
 
 	filter := bson.M{"_id": bson.M{"$eq": _id}}
-	change := bson.M{"$push": bson.M{"results": marketData.Results[0]}}
+	resultsChange := bson.M{"$push": bson.M{"results": result}}
 
-	_, err := marketDataCollection.UpdateOne(ctx, filter, change)
+	_, err := marketDataCollection.UpdateOne(ctx, filter, resultsChange)
 
 	if err != nil {
 		log.Println("Error updating document. " + err.Error())
